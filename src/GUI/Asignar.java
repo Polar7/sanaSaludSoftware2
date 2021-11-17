41d7b3287d36f2b818a88029b64ff6997078586e
fenix <juanmaximo@gmail.com>
2021-11-16 18:42:55 -0600

    GUI creation

---
+++ src/GUI/Asignar.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javax.swing.*;
+
+public class Asignar {
+    private JTextField cedula;
+    private JButton buscarUsuario;
+    private JTextField nombre;
+    private JTextField opto;
+    private JTextField oto;
+    private JTextField psico;
+    private JTextField gene;
+    private JLabel Cedulalab;
+}
---
+++ src/GUI/Recepcion.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import javax.swing.*;
+
+public class Recepcion {
+
+
+    private JPanel Registrar;
+    private JTextField nombre;
+    private JTextField cedula;
+    private JTextField edad;
+    private JTextField sexo;
+    private JTextField direccion;
+    private JButton registrar;
+    private JButton limpiar;
+    private JButton refrescar;
+    private JTable grilla;
+
+}
