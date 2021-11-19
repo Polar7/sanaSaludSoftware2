8a95c74402d1e7e89230770b4c2d522894f34e9b
Jean Lozano <michecoanexa52@gmail.com>
2021-11-18 23:35:36 -0500

    Add libraries to GUI

--- src/GUI/Menu.java
+++ src/GUI/Menu.java
@@ -1,10 +1,41 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Menu {
     private JButton examenes;
     private JButton asignaciones;
     private JButton recepcion;
     private JPanel menu;
+
+    public Menu() {
+        examenes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        recepcion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        asignaciones.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Sana Salud");
+        frame.setContentPane(new Menu().menu);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
